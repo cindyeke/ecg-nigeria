ff9798eadcae8305435e44b88ac64451ed9af51c
Cindy Eke <officialcee1@gmail.com>
2025-01-27 13:20:01 +0100

    update contact pagr copy style

--- src/app/contactus/page.js
+++ src/app/contactus/page.js
@@ -4,7 +4,7 @@ import Layout from '@components/template/Layout'
 export default function ContactUs() {
     return (
         <Layout hasPageBanner>
-            <p className="mt-10 px-5 text-lg md:px-10">
+            <p className="mt-10 px-5 text-xl md:px-10 xl:px-20 xl:text-2xl max-w-[650px]">
                 Feel free to contact us using the phone numbers provided on each
                 branch page or send us a message.
             </p>
